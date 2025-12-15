42eaa88bcf117d73a0986405d6e969cef56858ad
Swann Julien <swann.jn@gmail.com>
2025-12-15 22:39:40 +0100

    styles: change spinner coulor

--- src/components/spinner-element.js
+++ src/components/spinner-element.js
@@ -12,7 +12,7 @@ export class Spinner extends LitElement {
     .spinner {
       width: 40px;
       height: 40px;
-      border: 4px solid rgba(0, 0, 0, 0.2);
+      border: 4px solid rgba(52, 211, 153, 1);
       border-top: 4px solid black;
       border-radius: 50%;
       animation: spin 0.8s linear infinite;
